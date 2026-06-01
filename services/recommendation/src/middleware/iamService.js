4710eee0145a784bd8cd9d5139075e95e8364a02
Username148576 <jenny20030314@gmail.com>
2026-06-02 04:56:02 +0800

    test

--- services/recommendation/src/middleware/iamService.js
+++ services/recommendation/src/middleware/iamService.js
@@ -2,10 +2,11 @@ const getfactoryZoneByEmployeeId = async (headers, employeeId) => {
   const userId = headers['x-user-id'];
   const userRole = headers['x-user-role'];
   const userEmail = headers['x-user-email'];
-  const url = new URL(`127.0.0.1:3001/employees/user/${employeeId}`);
+  const url = new URL(`http://127.0.0.1:3001/employees/user/${employeeId}`);
   const res = await fetch(url.toString(), {
     method: 'GET',
     headers: {
+      "Content-Type": "application/json",
       'X-User-ID': userId,
       'X-User-Role': userRole,
       'X-User-Email': userEmail
